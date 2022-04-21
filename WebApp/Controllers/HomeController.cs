0a631f4c7544818de19e360e9b371d3656fc9507
fad16papa <fad16papa@gmail.com>
2022-04-21 12:02:25 +0800

    Commit changes

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -7,14 +7,17 @@ namespace WebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            ViewBag.Data = _configuration["Test"];
             return View();
         }
 
